ebd2693edad457ab9529cb3adc87b0300b88bdc6
Barry Richards <barry@br-web.me>
2025-07-05 17:17:08 +0100

    Config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,9 @@
-export default async function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("assets");
-};
+export default function(eleventyConfig) {
+
+  return {
+    dir: {
+      input: "src",       // use current directory as input
+      output: "_site"   // default output directory
+    }
+  };
+}
